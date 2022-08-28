46b44f348ccd01870e709ab00e64c2a62fe9fd4e
Stepa075 <stepa0751@gmail.com>
2022-08-29 01:19:59 +0300

    With data from get read and write

--- pythonFlask.py
+++ pythonFlask.py
@@ -1,15 +1,34 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/<username>')
 # def show_user_profile(username):
-#     # показать профиль данного пользователя
 #     return 'Hello my good friend %s' % username
-def show_user_profile(username):
-    # показать профиль данного пользователя
-    return f'Hello my good friend { username } I love you!'
+def show_user_name(username):
+    return f'Hello my good friend {username} I love you!'
+
+
+@app.route("/data", methods=['GET', ])
+def setinfo():
+    # Получить значение параметра id во входящем URL http://localhost:5000/data?id=666&data=123456
+    sendid = request.args.get('id')
+    getdata = request.args.get('data')
+    with open("data.txt", "w") as f:
+        f.write(getdata)
+    return "Get info id is " + str(sendid)
+
+
+@app.route("/data1", methods=['GET', ])
+def getinfo():
+    #  URL http://localhost:5000/data1?id=666&data=123456
+    # sendid = request.args.get('id')
+    # getdata = request.args.get('data')
+    with open("data.txt", "r") as f:
+        z = f.read()
+    return "Get info data is " + str(z)
 
 
 if __name__ == "__main__":
